c35f49dbc852120e8251f68681e5edcfd3b325bd
Richiewebs <asmahrichelle9@gmail.com>
2024-05-19 16:03:32 +0000

    just saving

--- client/src/pages/SmsHistory.jsx
+++ client/src/pages/SmsHistory.jsx
@@ -23,7 +23,7 @@ const SmsHistory = () => {
                         <a href='#'>Undelivered</a>
                     </div>
                     <div className='flex space-x-10 pb-3 '>
-                        <input type="search" name="" id="" placeholder='Search' className='px-4 rounded-md border-2 border-timecolor relative'/><img src={Img2} alt="" className='absolute top-2 pl-40' />
+                        <input type="text" name="" id="" placeholder='Search' className='px-4 rounded-md border-2 outline-none border-timecolor relative'/><img src={Img2} alt="" className='absolute top-2 pl-40' />
                         <div className='w-10 bg-timecolor py-1'><img src={Img} alt="" className='m-auto' /></div>
                     </div>
                 </div>
