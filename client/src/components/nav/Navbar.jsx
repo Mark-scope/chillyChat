a806f391be5916c3a246545173c3aa2a57c2ee59
Mark-scope <agyemangmark1@gmail.com>
2024-05-22 13:09:37 +0000

    feat: fixed the navbar

--- client/src/components/nav/Navbar.jsx
+++ client/src/components/nav/Navbar.jsx
@@ -2,9 +2,18 @@ import Balance from "./Balance"
 import Profile from "./Profile"
 function Navbar(){
     return(
-        <nav className="w-full h-20 bg-white z-10 shadow-md flex">
-            <Balance></Balance>
-            <Profile name="John"></Profile>
+        <nav className="w-full h-20 bg-white z-10 shadow-md flex  items-center px-12">
+           
+            <div className="w-1/2">                
+                <h1 className=' text-xl text-violet '>
+                <span className='font-bold'>Chilly</span>chat
+                </h1>
+            </div>
+
+            <div className="flex justify-end items-center w-1/2">
+                <Balance></Balance>
+                <Profile name="John"></Profile>
+            </div>
 
         </nav>
     )
--- client/src/components/nav/smshistorynav.jsx
+++ client/src/components/nav/smshistorynav.jsx
@@ -5,11 +5,17 @@ import Profile from "./Profile"
 const Smshistorynav = () => {
   return (
     
-      <nav className="w-full h-20 bg-white z-10 shadow-md flex">
-            <h1 className='pl-10 pt-5 text-xl text-violet'><span className='font-bold'>Chilly</span>chat</h1>
-            <Balance></Balance>
-            <Profile name="John"></Profile>
+      <nav className="w-full h-20 bg-white z-10 shadow-md flex  items-center px-20">
+            <div className="w-1/2 ">                
+                <h1 className=' text-xl text-violet '>
+                <span className='font-bold'>Chilly</span>chat
+                </h1>
+            </div>
 
+            <div className="flex justify-end items-center w-1/2">
+                <Balance></Balance>
+                <Profile name="John"></Profile>
+            </div>
         </nav>
    
   )
