edf2256724733d370bfec8ed8c806e5c22ddf922
Richiewebs <asmahrichelle9@gmail.com>
2024-05-23 04:56:48 +0000

    changes the margin of the smshistory view

--- client/src/pages/SmsHistoryView.jsx
+++ client/src/pages/SmsHistoryView.jsx
@@ -8,11 +8,8 @@ const SmsHistoryView = () => {
     <div>
       <Smshistorynav></Smshistorynav>
 
-      <div className='w-28 flex p-3 border-y-2 border-idcolor m-7 ml-20'>
-        <a className='flex' href='#'><img src={Img} alt="" />Back</a>
-
-      </div>
-      < p className='text-xl pl-20 text-viewtext'>Sms History</p>
+      
+      <div className='mt-10'>< p className='text-xl pl-20 text-viewtext'>Sms History</p></div>
       <div className='flex'>
         <div className='ml-20 mt-5 w-2/4 h-auto'>
           <table className='w-4/5 py-5 bg-transparent'>
@@ -34,12 +31,12 @@ const SmsHistoryView = () => {
             </tbody>
           </table>
         </div>
-        <div className='flex-col w-2/4 text-divtext'>
-          <div className='w-521 p-4  bg-white shadow-md rounded-lg mb-5'>
+        <div className='flex-col w-2/4 m-auto text-divtext'>
+          <div className='w-521 p-4  bg-white m-auto shadow-md rounded-lg mb-5'>
             <h1 className='border-b border-smsborder text-viewtext font-bold py-3'>Subject</h1>
             <p className='py-5'>Come for momo</p>
           </div>
-          <div className='w-521 p-4 bg-white shadow-md rounded-lg'>
+          <div className='w-521 p-4 bg-white m-auto shadow-md rounded-lg'>
           <h1 className='border-b border-smsborder text-viewtext font-bold py-3'>Message</h1>
           <p className='py-5'>Lorem ipsum dolor sit amet,
           consectetur adipiscing elit, sed do eiusmod tempor
@@ -50,12 +47,17 @@ const SmsHistoryView = () => {
           in voluptate velit esse cillum dolore eu fugiat 
           </p>
           </div>
-          <div className='mt-2'>
+          <div className='mt-2 ml-24 '>
             <h1>Time:</h1>
-            <div className='bg-timecolor w-44 text-center text-white rounded p-1'>{new Date().toLocaleString()}</div>
+            <div className='bg-timecolor w-52 text-center text-white rounded p-1'>{new Date().toLocaleString()}</div>
           </div>
         </div>
       </div>
+      <div className='w-28 flex p-3  border-y-2 border-idcolor m-7 ml-20'>
+        <a className='flex' href='#'><img src={Img} alt="" />Back</a>
+
+      </div>
+    
     </div>
 
   )
