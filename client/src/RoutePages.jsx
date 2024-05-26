19fa58401ff331c766ef749c1af94729e009bea8
Mark-scope <agyemangmark1@gmail.com>
2024-05-26 13:11:50 +0000

    feat: completion of login Page

--- client/src/RoutePages.jsx
+++ client/src/RoutePages.jsx
@@ -6,6 +6,7 @@ import SmsHistory from './pages/SmsHistory';
 import SmsHistoryView from './pages/SmsHistoryView';
 import Bulksms from './pages/Bulksms';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 const RoutePages = () => {
     return (
@@ -19,6 +20,7 @@ const RoutePages = () => {
                 <Route path = "/SmsHistoryView" element={<SmsHistoryView />}/>
                 <Route path = "/Bulksms" element={<Bulksms />}/>
                 <Route path = "/Signup" element={<Signup />}/>
+                <Route path = "/Login" element={<Login />}/>
 
             </Routes>
         </BrowserRouter>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import Loginicon from '../assets/login.svg'
+import google from '../assets/google.png'
 
 const Login = () => {
   return (
     <div className='flex'>
-
+      {/* Side with image */}
         <div className='w-1/2 text-white flex flex-col justify-center items-center bg-custom-gradient h-screen'>
             <img className='pb-12 pr-16' src={Loginicon} alt="" />
             <p className='font-semibold text-[20px]' >Connect  With Your Target With a Click</p>
             <span className=' pt-4'>Everything you need is easy and simple to use in dashboard</span>
         </div>
 
-        <div className='w-1/2 flex flex-col justify-center items-center space-y-10 '>
+      {/* main content */}
+        <div className='w-1/2 flex flex-col justify-center items-center space-y-10 p-12 '>
             <div className='flex flex-col justify-center items-center '>
                 <h1 className=' text-xl text-violet '>
                 <span className='font-bold'>Chilly</span>Chat
@@ -29,22 +32,23 @@ const Login = () => {
                 <div className="flex-1 h-1 bg-black"></div>
             </div>
 
-            <div >
+            <div className='w-[404px]'>
                 
                 <form action="" className=' flex flex-col'>
-                    <div className='flex space-x-4 w-full '>
-                        <input type="text" placeholder='First Name' className='mb-6 w-70% border-b border-gray-300 p-2 outline-none bg-[#F9FAFB]' />
-                        <input type="text" placeholder='Last Name' className=' w-70% mb-6 border-b border-gray-300 p-2 outline-none bg-[#F9FAFB]' />
-
-                    </div>
+                    
                     <input type="text" placeholder='Email' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
                     <input type="text" placeholder='Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
-                    <input type="text" placeholder='Confirm Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 bg-[#F9FAFB] outline-none' />
-                    <button className="px-[16px] mt-8 h-12 bg-navcolor text-white font-sans  rounded-lg">Sign</button>
+                    
+                    <div>
+                        <input type="checkbox" className='mr-2' id="remember" name="remember" />
+                        <label class="form-check-label" for="remember">Remember Me</label>
+                    </div>
+                    
+                    <button className="px-[16px] mt-8 h-12 bg-navcolor text-white font-sans  rounded-lg">Login</button>
 
                 </form>
 
-                <p className='text-center mt-4'>Already have an account? <a className='text-violet'>Login now</a></p>
+                <p className='text-center mt-4'>Don't have an account? <a className='text-violet'>Click here</a></p>
             </div>
 
             
