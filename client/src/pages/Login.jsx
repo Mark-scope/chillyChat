e02de4b17f59172ddd15c762f71e84b89e35a6fa
Richiewebs <asmahrichelle9@gmail.com>
2024-06-09 22:39:29 +0000

    done with the signup and login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,69 +1,72 @@
-import React from 'react'
-import Loginicon from '../assets/login.svg'
-import google from '../assets/google.png'
+import React, { useState, useEffect } from 'react';
+import Loginicon from '../assets/login.svg';
+import google from '../assets/google.png';
+import hand from "../assets/8.png";
 
 const Login = () => {
+  const [currentImage, setCurrentImage] = useState(Loginicon);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage === Loginicon ? hand : Loginicon));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className='flex'>
       {/* Side with image */}
-        <div className='w-1/2 text-white flex flex-col justify-center items-center bg-custom-gradient h-screen'>
-            <img className='pb-12 pr-16 mt-10' src={Loginicon} alt="" />
-            <p className='font-semibold text-[20px]' >Connect  With Your Target With a Click</p>
-            <span className=' pt-4'>Everything you need is easy and simple to use in dashboard</span>
-                <div id='circles' className='w-32  m-auto flex space-x-3'>
-                    <div id='box'>
-                        <span class = "circle"></span>
-                        <span class = "circle"></span>
-                    </div>
-                </div>
-               </div>
+      <div className='w-1/2 text-white flex flex-col justify-center items-center bg-custom-gradient h-screen'>
+        <img className='pb-12 pr-16 mt-10' src={currentImage} alt="" />
+        <p className='font-semibold text-[20px]'>Connect With Your Target With a Click</p>
+        <span className='pt-4'>Everything you need is easy and simple to use in dashboard</span>
+        <div id='circles' className='w-32 m-auto flex space-x-3'>
+          <div id='box'>
+            <span className="circle"></span>
+            <span className="circle"></span>
+          </div>
+        </div>
+      </div>
 
       {/* main content */}
-        <div className='w-1/2 flex flex-col justify-center items-center space-y-10 p-12 '>
-            <div className='flex flex-col justify-center items-center '>
-                <h1 className=' text-xl text-violet '>
-                <span className='font-bold'>Chilly</span>Chat
-                </h1>
+      <div className='w-1/2 flex flex-col justify-center items-center space-y-10 p-12'>
+        <div className='flex flex-col justify-center items-center'>
+          <h1 className='text-xl text-violet'>
+            <span className='font-bold'>Chilly</span>Chat
+          </h1>
 
-                <p>Create an account with us</p>
-            </div>
-
-            <img src={google} alt="" />
-
-            <div className="flex flex-row items-center">
-                <div className="flex-1 h-1 bg-black"></div>
-                <div className="w-70 text-center">
-                    <p>Example</p>
-                </div>
-                <div className="flex-1 h-1 bg-black"></div>
-            </div>
-
-            <div className='w-[404px]'>
-                
-                <form action="" className=' flex flex-col'>
-                    
-                    <input type="text" placeholder='Email' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
-                    <input type="text" placeholder='Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
-                    
-                    <div>
-                        <input type="checkbox" className='mr-2' id="remember" name="remember" />
-                        <label class="form-check-label" for="remember">Remember Me</label>
-                    </div>
-                    
-                    <button className="px-[16px] mt-8 h-12 bg-navcolor text-white font-sans  rounded-lg">Login</button>
-
-                </form>
-
-                <p className='text-center mt-4'>Don't have an account? <a className='text-violet'>Click here</a></p>
-            </div>
-
-            
-
-
+          <p>Create an account with us</p>
         </div>
 
+        <img src={google} alt="" />
+
+        <div className="flex flex-row items-center">
+          <div className="flex-1 h-1 bg-black"></div>
+          <div className="w-70 text-center">
+            <p className="exam">Example</p>
+          </div>
+          <div className="flex-1 h-1 bg-black"></div>
+        </div>
+
+        <div className='w-[404px]'>
+          <form action="" className='flex flex-col'>
+            <input type="text" placeholder='Email' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
+            <input type="text" placeholder='Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
+
+            <div>
+              <input type="checkbox" className='mr-2' id="remember" name="remember" />
+              <label className="form-check-label" htmlFor="remember">Remember Me</label>
+            </div>
+
+            <button className="px-[16px] mt-8 h-12 bg-navcolor text-white font-sans rounded-lg">Login</button>
+          </form>
+
+          <p className='text-center mt-4'>Don't have an account? <a href='/Signup' className='text-violet cursor-pointer'>Click here</a></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,65 +1,70 @@
-import React from 'react'
-import Loginicon from '../assets/login.svg'
-import google from '../assets/google.png'
-import Button from '../components/Button'
+import React, { useState, useEffect } from 'react';
+import Loginicon from '../assets/login.svg';
+import google from '../assets/google.png';
+import hand from "../assets/8.png";
+// import Button from '../components/Button';
 
 const Signup = () => {
+  const [currentImage, setCurrentImage] = useState(Loginicon);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage === Loginicon ? hand : Loginicon));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className='flex'>
+      <div className='w-1/2 text-white flex flex-col justify-center items-center bg-custom-gradient h-screen'>
+        <img className='pb-12 pr-16 mt-10' src={currentImage} alt="" />
+        <p className='font-semibold text-[20px]'>Connect With Your Target With a Click</p>
+        <span className='pt-4'>Everything you need is easy and simple to use in dashboard</span>
+        <div id='circles' className='w-32 m-auto flex space-x-3'>
+          <div id='box'>
+            <span className="circle"></span>
+            <span className="circle"></span>
+          </div>
+        </div>
+      </div>
 
-        <div className='w-1/2 text-white flex flex-col justify-center items-center bg-custom-gradient h-screen'>
-            <img className='pb-12 pr-16 mt-10' src={Loginicon} alt="" />
-            <p className='font-semibold text-[20px]' >Connect  With Your Target With a Click</p>
-            <span className=' pt-4'>Everything you need is easy and simple to use in dashboard</span>
-            <div id='circles' className='w-32  m-auto flex space-x-3'>
-                    <div id='box'>
-                        <span class = "circle"></span>
-                        <span class = "circle"></span>
-                    </div>
-                </div>
+      <div className='w-1/2 flex flex-col justify-center items-center space-y-10'>
+        <div className='flex flex-col justify-center items-center'>
+          <h1 className='text-xl text-violet'>
+            <span className='font-bold'>Chilly</span>Chat
+          </h1>
+
+          <p>Create an account with us</p>
         </div>
 
-        <div className='w-1/2 flex flex-col justify-center items-center space-y-10 '>
-            <div className='flex flex-col justify-center items-center '>
-                <h1 className=' text-xl text-violet '>
-                <span className='font-bold'>Chilly</span>Chat
-                </h1>
+        <img src={google} alt="" />
 
-                <p>Create an account with us</p>
-            </div>
-
-            <img src={google} alt="" />
-
-            <div className="flex flex-row items-center">
-                <div className="flex-1 h-1 bg-black"></div>
-                <div className="w-70 text-center">
-                    <p >Example</p>
-                </div>
-                <div className="flex-1 h-1 bg-black"></div>
-            </div>
-
-            <div >
-                
-                <form action="" className=' flex flex-col'>
-                    <div className='flex space-x-4 w-full '>
-                        <input type="text" placeholder='First Name' className='mb-6 w-70% border-b border-gray-300 p-2 outline-none bg-[#F9FAFB]' />
-                        <input type="text" placeholder='Last Name' className=' w-70% mb-6 border-b border-gray-300 p-2 outline-none bg-[#F9FAFB]' />
-
-                    </div>
-                    <input type="text" placeholder='Email' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
-                    <input type="text" placeholder='Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
-                    <input type="text" placeholder='Confirm Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 bg-[#F9FAFB] outline-none' />
-                    <button className="px-[16px] mt-8 h-12 bg-navcolor text-white font-sans  rounded-lg">Sign</button>
-
-                </form>
-
-                <p className='text-center mt-4'>Already have an account? <a className='text-violet'>Login now</a></p>
-            </div>
-
+        <div className="flex flex-row items-center">
+          <div className="flex-1 h-1 bg-black"></div>
+          <div className="w-70 text-center">
+            <p className="exam">Example</p>
+          </div>
+          <div className="flex-1 h-1 bg-black"></div>
         </div>
 
+        <div>
+          <form action="" className='flex flex-col'>
+            <div className='flex space-x-4 w-full'>
+              <input type="text" placeholder='First Name' className='mb-6 w-70% border-b border-gray-300 p-2 outline-none bg-[#F9FAFB]' />
+              <input type="text" placeholder='Last Name' className='w-70% mb-6 border-b border-gray-300 p-2 outline-none bg-[#F9FAFB]' />
+            </div>
+            <input type="text" placeholder='Email' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
+            <input type="text" placeholder='Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 outline-none bg-[#F9FAFB]' />
+            <input type="text" placeholder='Confirm Password' className='w-70% mb-6 border-b border-navcolor-300 p-2 bg-[#F9FAFB] outline-none' />
+            <button className="px-[16px] mt-8 h-12 bg-navcolor text-white font-sans rounded-lg">Sign</button>
+          </form>
+
+          <p className='text-center mt-4'>Already have an account? <a href="/Login" className='text-violet cursor-pointer'>Login now</a></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
