537ceb78dac2c26c91862ef00ca8f0a8a220e5fb
Richiewebs <asmahrichelle9@gmail.com>
2024-06-13 15:15:42 +0000

    added the icons to the menu

--- client/src/components/sidebar/Menu.jsx
+++ client/src/components/sidebar/Menu.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
+import chart from "../../assets/chart-2.png";
+import call from "../../assets/call.png";
+import graph from "../../assets/graph.png";
+import message from "../../assets/messages.png";
+import headphone from "../../assets/headphones.png";
 
 function Lists(props) {
   return (
-    <li className="w-full h-14 text-center pt-4 hover:bg-white hover:text-violet">
-      <a href="#" className="h-full p-5 px-11 text-white hover:text-violet" onMouseEnter={props.onHover}>
+    <li className="w-full flex h-14 hover:bg-white hover:text-violet" id="list">
+      <div className='mt-2 pl-16 '><img className="w-10" src={props.image} alt={props.item} /></div>
+      <a href="#" className="w-full pl-4 h-full p-5 px-5 pl-0  text-white hover:text-violet" onMouseEnter={props.onHover}>
         {props.item}
       </a>
     </li>
@@ -22,7 +28,7 @@ function Sublists(props) {
 
 function Menus() {
   const subitems = ['Quick sms', 'Bulk sms', 'Sms History'];
-  
+  const images = [chart, call, message, graph, headphone];
   const items = ['Overview', 'Contacts', 'Sms', 'Report', 'Support'];
   const [showSubitems, setShowSubitems] = useState(false);
 
@@ -35,7 +41,7 @@ function Menus() {
       <ul>
         {items.map((item, index) => (
           <React.Fragment key={index}>
-            <Lists item={item} onHover={() => handleHover(index)} />
+            <Lists item={item} image={images[index]} onHover={() => handleHover(index)} />
             {index === 2 && showSubitems && ( // Render subitems directly below the third item
               <div className="w-full h-auto">
                 {subitems.map((subitem, subIndex) => (
@@ -50,8 +56,4 @@ function Menus() {
   );
 }
 
-
-
-
-
-export default Menus
+export default Menus;
