5d296131fc38451ecc1b768431af4b83fab49f93
Richiewebs <asmahrichelle9@gmail.com>
2024-06-24 12:29:36 +0000

    added the mongoose and bcrypt modules

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const{ connectToDb, getDb } = require('./db')
+
+//db connection
+let db
+connectToDb((err)=>{
+  if(!err){
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+    db = getDb()
+  }
+  
+})
 
 app.get('/', (req, res) => {
+  db.collection('SignUp')
+  .find()
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+
